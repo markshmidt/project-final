6ca61a9be0c5b47ee3c2be4056b81ee077ef4ae7
markshmidt <masha.shmidt.04@gmail.com>
2025-07-02 18:22:32 -0400

    rewrite FileUtil.upload using Path and Files API

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -15,26 +15,28 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @UtilityClass
 public class FileUtil {
     private static final String ATTACHMENT_PATH = "./attachments/%s/";
-
     public static void upload(MultipartFile multipartFile, String directoryPath, String fileName) {
         if (multipartFile.isEmpty()) {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
-            }
+        Path dirPath = Paths.get(directoryPath);
+        Path filePath = dirPath.resolve(fileName);
+
+        try {
+            Files.createDirectories(dirPath);
+            Files.write(filePath, multipartFile.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException ex) {
+            throw new IllegalRequestDataException("Failed to upload file: " + multipartFile.getOriginalFilename());
         }
     }
+
+
 
     public static Resource download(String fileLink) {
         Path path = Paths.get(fileLink);
