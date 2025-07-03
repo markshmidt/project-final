74266c7fd669c535c5d1240f7c36c62fcb877635
markshmidt <masha.shmidt.04@gmail.com>
2025-07-03 12:31:15 -0400

    tests for ProfileRestController and fix MockMvc setup

--- src/test/java/com/javarush/jira/AbstractControllerTest.java
+++ src/test/java/com/javarush/jira/AbstractControllerTest.java
@@ -1,21 +1,28 @@
 package com.javarush.jira;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+@SpringBootTest
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications
 @Sql(scripts = {"classpath:db/changelog.sql", "classpath:data.sql"}, config = @SqlConfig(encoding = "UTF-8"))
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications-testing-with-mock-environment
 public abstract class AbstractControllerTest extends BaseTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     protected ResultActions perform(MockHttpServletRequestBuilder builder) throws Exception {
         return mockMvc.perform(builder);
--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,11 +1,87 @@
 package com.javarush.jira.profile.internal.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javarush.jira.profile.ContactTo;
+import com.javarush.jira.profile.ProfileTo;
 import com.javarush.jira.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Set;
+
+import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class ProfileRestControllerTest extends AbstractControllerTest {
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    private static final String REST_URL = ProfileRestController.REST_URL;
+
+    @Autowired
+
+    private MockMvc mockMvc;
+
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void getProfile_success() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void getProfile_unauthorized() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void updateProfile_success() throws Exception {
+        ProfileTo updated = new ProfileTo(1L,
+                Set.of("DAILY_DIGEST", "TASK_REMINDER"),
+                Set.of(new ContactTo("tg", "@user")));
+
+        mockMvc.perform(put(REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void updateProfile_invalid() throws Exception {
+        ProfileTo invalid = new ProfileTo(1L, null, null); // нарушает @NotNull
+
+        mockMvc.perform(put(REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalid)))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void updateProfile_unauthorized() throws Exception {
+        ProfileTo updated = new ProfileTo(1L,
+                Set.of("TASK_REMINDER"),
+                Set.of(new ContactTo("tg", "@anon")));
+
+        mockMvc.perform(put(REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated)))
+                .andExpect(status().isUnauthorized());
+    }
 }
